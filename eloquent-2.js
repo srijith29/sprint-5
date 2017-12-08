fa892f186a511e0d3fede6b68773479abb771296
srijith29 <srijith29@gmail.com>
2017-12-08 13:34:16 +1300

    adding blue-green dots

---
+++ eloquent-2.js
@@ -0,0 +1,18 @@
+var a ="";
+while(a.length<7)
+  console.log(a+="#");
+  ///////////////////////
+
+for(i=1;i<=100;i++)
+{
+  if(i%3==0&&i%5==0)
+    console.log("FizzBuzz");
+if(i%3==0)
+console.log("Fizz");
+if(i%5==0)
+console.log("Buzz");
+  else
+console.log(i);
+}
+
+////////////////////////////////////
