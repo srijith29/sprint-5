3bda8e5f27cc9bff60ce044996ebca6b79474320
srijith29 <srijith29@gmail.com>
2017-12-10 21:20:28 +1300

    adding eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,8 +1,10 @@
+//looping a triangle
 var a ="";
 while(a.length<7)
   console.log(a+="#");
   ///////////////////////
 
+//FIZZBUZZ
 for(i=1;i<=100;i++)
 {
   if(i%3==0&&i%5==0)
@@ -14,5 +16,19 @@ console.log("Buzz");
   else
 console.log(i);
 }
-
 ////////////////////////////////////
+
+//chessboard
+var a = "";
+for(i=0;i<8;i++)
+{
+  for(j=0;j<8;j++)
+  {
+    if((i+j)%2==0)
+      a+="#";
+    else
+      a+=" ";
+  }
+ a+="\n";
+}
+console.log (a);
